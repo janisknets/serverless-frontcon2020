27a9f8b1109dbb618fb2d462e1256de1bdb73ba4
Janis Knets <janis.knets@accenture.com>
2020-08-17 11:48:48 +0300

    Added post for users

--- handlers/users/postUsers.ts
+++ handlers/users/postUsers.ts
@@ -3,7 +3,15 @@ import * as Responses from '../../utils/commonResponses';
 import User from '../../dynamo/users';
 
 export default async (event: APIGatewayProxyEvent, context: Context, callback: Callback) => {
-  const users: any[] = await User.scan().exec();
 
-  return Responses.ok200({ users });
+  try {
+    const parsedBody = JSON.parse(event.body);
+
+    const user: any = await User.create(parsedBody);
+
+    return Responses.ok200({ user });
+  } catch (error) {
+    console.error(error);
+    return Responses.internalError(error);
+  }
 }
